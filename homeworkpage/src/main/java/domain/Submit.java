5d710566778219773522f215378a8b22b2f1674b
yaaan <wangpajeon@gmail.com>
2024-07-29 06:56:55 +0900

    submit 추가

--- homeworkpage/src/main/java/domain/Submit.java
+++ homeworkpage/src/main/java/domain/Submit.java
@@ -1,4 +1,17 @@
 package domain;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Submit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String content;
 }
